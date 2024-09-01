04f0aa5c315f24e3247d6d7e507d17edb0294afc
srabonty2249 <jarintasnim0616@gmail.com>
2024-09-01 18:17:00 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* =============JQ============*/
+$(document).ready(function(){
+      
+      alert("hello");
+      //counterup
+      $('.counter').counterUp({
+      	delay:10,
+      	time:1000
+      });
+      $("#owl-demo").owlCarousel({
+    navigation : true
+  });
+
+})
+
